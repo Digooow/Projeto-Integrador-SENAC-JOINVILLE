95cea6299fe7c54753ace4f6ed0f55b1926e7016
Arka <digo13045@gmail.com>
2026-05-24 15:12:26 -0300

    Atualizado Trabalho integrador TEMPLATE

--- WpfApp/obj/Debug/net8.0-windows/WpfApp.AssemblyInfo.cs
+++ WpfApp/obj/Debug/net8.0-windows/WpfApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce1186386c9b3df7d369a8ec126b73f1838bcf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93b40ca3582a04b00994c2bec26836ace4e56f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
